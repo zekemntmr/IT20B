4040829e3f3a973c68251430a5239328d71ca072
zekemntmr <zekemntmr@gmail.com>
2025-11-18 08:08:12 +0800

    added math.js

--- math.js
+++ math.js
@@ -1,1 +1,38 @@
-ag
+//num1 = 2num2
+//b = 3;
+
+//sum = num1 + bnum2
+//diff = num1 - bnum2
+//quo = num1 / bnum2
+//prod = num1 * bnum2
+
+//console.log("the sum of num1 numnum2nd bnum2is " + sum);
+//console.log("the difference of num1 numnum2nd bnum2is "+ diff);
+//console.log("the product of num1 numnum2nd bnum2is " + prod);
+//console.log("the quotient of num1 numnum2nd bnum2is" + quo);
+
+
+function sum (){
+    sum = num1  + num2;
+    console.log(sum);
+}
+
+diff(2,3)
+
+function diff (num1,num2){
+    diff = num1  - num2;
+    console.log(diff);
+}
+prod(2,3)
+
+function prod (num1,num2){
+    prod = num1  * num2;
+    console.log(prod);
+}
+
+quo(2,3)
+
+function quo (num1,num2){
+    quo = num1  / num2;
+    console.log(quo);
+}
