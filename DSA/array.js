d8cca1cb6c57352aff7808c4881e2b37d344097d
zekemntmr <zekemntmr@gmail.com>
2025-11-25 08:06:31 +0800

    second commit

--- DSA/array.js
+++ DSA/array.js
@@ -21,11 +21,28 @@ class Array{
             output += this.data + "";
         }
         console.log(output);
+    }
+    insert(index,element){
+        //Correcting Statement
+        if(index<0 || index > this.length){
+            console.log("Invalid Index");
+            return;
+        }
 
+        //Shifting Condition
+        for(let i = this.length; i > index; i--){
+            this.data[i] = this.data[i-1];
+        }
+
+        //After Condition
+        this.data[index] = element;
+        this.length++;
 
     }
 }
 
 arr1 = new Array9[4,3,2];
 
+arr1.traverse();
+arr1.insert(0,9);
 arr1.traverse();
