e4aace27680c80360464c30723a5e3da49b3bf29
zekemntmr <zekemntmr@gmail.com>
2025-11-25 08:48:35 +0800

    added pop method for stack

--- DSA/stack.js
+++ DSA/stack.js
@@ -23,16 +23,25 @@ class Stack{
     }
     is_empty(){
         return this.topIndex === -1;
-
-        }
+    }
     size(){
         return this.topIndex+1
-        }
+    }
 
     peek(){
         if(this.is_empty()) return null;
         return this.data[this.topIndex];
-        }
+    }
+    pop(){
+        if(this.is_empty()) return null;
+        let value = this.data[this.topIndex];
+        delete this.data[this.topIndex];
+        this.topIndex--;
+        return value;
+    }
+    
+
+
 }
 
 stack1 = new Stack();
@@ -40,5 +49,13 @@ stack1 = new Stack();
 stack1.push(4);
 stack1.push(2);
 stack1.push(3);
+stack1.push(5);
+stack1.push(3);
+
+console.log(stack1.is_empty());
+console.log(stack1.size());
+console.log("the top element is " + stack1.peek());
+console.log("top element" + stack1.pop() + " is popped, the new top element is "+ stack1.peek())
+console.log("the new stack size is " + stack1.size());
 
 stack1.traverse();
